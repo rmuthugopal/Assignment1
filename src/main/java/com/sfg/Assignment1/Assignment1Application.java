f3558c3bd1e541b8a96bdc412041c88f1efcc169
rmuthugopal <rmuthugopal@gmail.com>
2020-10-05 14:34:20 +0800

    first commit

---
+++ src/main/java/com/sfg/Assignment1/Assignment1Application.java
@@ -0,0 +1,14 @@
+package com.sfg.Assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1Application.class, args);
+		System.out.println("My First Spring Boot Application");
+	}
+
+}
